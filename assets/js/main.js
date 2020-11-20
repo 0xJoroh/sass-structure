19f97dfd866246fa567af419918cb57bee0b580b
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2020-11-20 11:17:06 +0100

    Latest Jquery, sass files cleaning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,20 @@
 $(() => {
+	// Enable Bootstrap tooltip
 	$('[data-toggle="tooltip"]').tooltip();
 });
 
-// Ripple Button Effect
-const root = document.documentElement;
-document.addEventListener("mousedown", ev => {
-	const el = ev.target,
-		top = 0,
-		left = 0;
-	do {
-		top += el.offsetTop;
-		left += el.offsetLeft;
-	} while (el = el.offsetParent);
-	const x = (ev.clientX - left) / ev.target.offsetWidth;
-	const y = (ev.clientY - top) / ev.target.offsetHeight;
-	root.style.setProperty("--ripple-x", x);
-	root.style.setProperty("--ripple-y", y);
-});
+// Ripple Button Effect, Get clicked x and y axise and feed the value to css variables (--ripple-x, --ripple-y)
+// const root = document.documentElement;
+// document.addEventListener("mousedown", ev => {
+// 	const el = ev.target,
+// 		top = 0,
+// 		left = 0;
+// 	do {
+// 		top += el.offsetTop;
+// 		left += el.offsetLeft;
+// 	} while (el = el.offsetParent);
+// 	const x = (ev.clientX - left) / ev.target.offsetWidth;
+// 	const y = (ev.clientY - top) / ev.target.offsetHeight;
+// 	root.style.setProperty("--ripple-x", x);
+// 	root.style.setProperty("--ripple-y", y);
+// });
